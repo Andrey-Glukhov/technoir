3830b3bd49e400256c2f619d487204d19e8b9fb3
Eliza <gluhova.liza@gmail.com>
2021-10-04 00:03:39 +0200

    menu

--- menu.php
+++ menu.php
@@ -105,7 +105,7 @@ get_header(); ?>
                             <p class="v">(V)</p>
                         </div>
                         <div class="col-6 savory">
-                        <p>SAVORY</p>
+                            <p>SAVORY</p>
                             <table>
                                 <tr>
                                     <td>jonge kaas</td>
@@ -172,31 +172,219 @@ get_header(); ?>
 
             <div class="row menu-row justify-content-around">
                 <div class="col-5">
-                    <img src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-09-min.png"> 
-                    <table>
-                    <tr>
-                         <td>gemarineerde feta (v) <br>
-                             rode koolsalsa, dukkah</td>
-                         <td> 9.0</td>
-                     </tr>
-                     <tr>
-                         <td>makreelsalade<br>
-                        mierikswortel, rode ui</td>
-                         <td>10.0</td>
-                     </tr>
-                     <tr>
-                         <td>hummus (VG) <br>
-                            ingemaakte groente, avocado</td>
-                         <td>10.0</td>
-                     </tr>
-                    </table>
-                    
+                    <div class="warm">
+                        <img
+                            src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-09-min.png">
+                        <table>
+                            <tr>
+                                <td>Homemade Burger <br>
+                                    - 100% beefpatty <br>
+                                    - korean fried chicken<br>
+                                    - vegan kikkererwt & rode biet (Vg)
+                                </td>
+                                <td> 13.5 </td>
+                            </tr>
+                        </table>
+                        <img src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-11.png">
+                        <table>
+                            <tr>
+                                <td>Rendang <br>
+                                    geserveerd met brood & pickles
+                                </td>
+                                <td>14.5</td>
+                            </tr>
+                            <tr>
+                                <td>Quiche (V) <br>
+                                    geserveerd met brood
+                                </td>
+                                <td>12.5</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="eggs">
+                        <p class="v">(V)</p>
+                        <table>
+                            <tr>
+                                <td span="1"><img
+                                        src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-13.png">
+                                </td>
+                                <td span="1"><span>8.5</span><br>
+                                    - scrambled<br>
+                                    - sunny side up<br>
+                                    - poached
+                                </td>
+                            </tr>
+                        </table>
+
+                    </div>
+
                 </div>
                 <div class="col-5 sandwiches">
                     <img src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-08-min.png">
+                    <table>
+                        <tr>
+                            <td>gemarineerde feta (v) <br>
+                                rode koolsalsa, dukkah</td>
+                            <td> 9.0</td>
+                        </tr>
+                        <tr>
+                            <td>makreelsalade<br>
+                                mierikswortel, rode ui</td>
+                            <td>10.0</td>
+                        </tr>
+                        <tr>
+                            <td>hummus (VG) <br>
+                                ingemaakte groente, avocado</td>
+                            <td>9.5</td>
+                        </tr>
+                        <tr>
+                            <td>kerrie kiprollade <br>
+                                lenteui, gebakken uitjes, sambal </td>
+                            <td>10.0</td>
+                        </tr>
+                        <tr>
+                            <td>pastrami <br>
+                                zuurkool,cheddar</td>
+                            <td>10.5</td>
+                        </tr>
+                        <tr>
+                            <td>geitenkaas (V) <br>
+                                uienchutney, pesto</td>
+                            <td>9.5</td>
+                        </tr>
+                        <tr>
+                            <td>pulled pork <br>
+                                cheddar, jalapeno, bbqsaus</td>
+                            <td>10.5</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 menu-divider">
+            <img src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-title-12.png">
+        </div>
+    </div>
+    <div class="row justify-content-center">
 
+        <div class="col-11">
+            <div class="row justify-content-around">
+                <div class="col-5">
+                    <div class="burger">
+                        <img src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-15.png">
+                        <table>
+                            <tr>
+                                <td>- 100% beefpatty<br>
+                                    - korean fried chicken<br>
+                                    - vegan kikkererwt - rode biet (VG)</td>
+                                <td>13.5</td>
+                            </tr>
+                        </table>
+                        <img class="seventy"
+                            src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-17.png">
+                    </div>
+                    <div class="fingerlicking_title">
+                        <img src="http://localhost:8888/technoir/wordpress/wp-content/themes/technoir/img/menu-16.png">
+                    </div>
+                    <div class="fingerlicking_content">
+                        <table>
+                            <tr>
+                                <td> spare ribs (700gr)<br>
+                                    thais gelakte ribbetjes, laos, chili
+                                </td>
+                                <td>16.5</td>
+                            </tr>
+                            <tr>
+                                <td> geroosterde bloemkool (VG) <br>
+                                    Hele bloemkool uit de oven met tahin, granaatappel & dukkah
+                                </td>
+                                <td>14.5</td>
+                            </tr>
+                            <tr>
+                                <td>dag special<br>
+                                    (kijk op onze screens)
+                                </td>
+                                <td>16.5</td>
+                            </tr>
+                            <tr>
+                                <td>geroosterde pompoen (V/VG)<br>
+                                    Hasselback flespompoen, tex-mex rub, sour cream
+                                </td>
+                                <td>14.5</td>
+                            </tr>
+                            <tr>
+                                <td>gegrild kippetje (400 gr)<br>
+                                    Bio poussin met sweet rub uit de oven
+                                </td>
+                                <td>15.0</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-5">
+                    <div class="snacks">
+                        <table>
+                            <tr>
+                                <td>vegan bitterballen (VG)</td>
+                                <td>7.5/13.0</td>
+                            </tr>
+                            <tr>
+                                <td>bitterballen </td>
+                                <td> 6.5/11.0</td>
+                            </tr>
+                            <tr>
+                                <td>kaastengel (V)</td>
+                                <td>6.5/11.0</td>
+                            </tr>
+                            <tr>
+                                <td>vlammetjes</td>
+                                <td>6.5/11.0</td>
+                            </tr>
+                            <tr>
+                                <td>Jalapeño poppers (V) </td>
+                                <td>7.5/13.0</td>
+                            </tr>
+                            <tr>
+                                <td>Korean Fried Chicken</td>
+                                <td>8.5/15.0</td>
+                            </tr>
+                            <tr>
+                                <td>bloemkool nuggets (VG)</td>
+                                <td>7.5/13.0</td>
+                            </tr>
+                            <tr>
+                                <td>chickenwings</td>
+                                <td>7.5/13.0</td>
+                            </tr>
+                            <tr>
+                                <td>nacho’s chilli</td>
+                                <td>12.5</td>
+                            </tr>
+                            <tr>
+                                <td>Chilli con carne, cheddar, sour creme, Jalapeño nacho’s (V/vg)</td>
+                                <td>11.5</td>
+                            </tr>
+                            <tr>
+                                <td>chili sin carne, avocado, salsa, Jalapeño olijven (VG) </td>
+                                <td>3.5</td>
+                            </tr>
+                            <tr>
+                                <td>brood met smeersels (v/vg)</td>
+                                <td>6.5</td>
+                            </tr>
+                            <tr>
+                                <td>kaasplank (V)</td>
+                                <td>13.5</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-1 dinner">
         </div>
 
     </div>
